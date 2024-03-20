668e883f39c35b52cc21444172e14cd9129499ed
Hameeda369 <hameedagm334@gmail.com>
2024-03-20 00:32:53 -0700

    Initial commit

--- 13_Yours Own _Array/Q#13.ts
+++ 13_Yours Own _Array/Q#13.ts
@@ -1,6 +1,6 @@
 //Question# 13
 
-let transportation_ : string[] = ['Motorcycle','Car','Train','Aeroplane'];
+let transPortation : string[] = ['Motorcycle','Car','Train','Aeroplane'];
 for(let i=0; i<transportation.length; i++){
 
     console.log('I would like to own  a ' + transportation[i])
